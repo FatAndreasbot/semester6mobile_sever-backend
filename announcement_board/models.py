a6a0efaa46452d67884c0f9a419314350c0f64aa
FatAndreasbot <guva.andrej.01@gmail.com>
2023-02-13 18:58:46 +0700

    created some models

--- announcement_board/models.py
+++ announcement_board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from common import models as common
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150, blank=True, default="")
+    author = models.ForeignKey(common.Profile, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    img = models.ImageField(upload_to="images/")
+    is_deleted = models.BooleanField(default=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- common/models.py
+++ common/models.py
@@ -2,3 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):	#in case we need some additional data for our user (profile picture, or something, i dunno)
+	authData = models.OneToOneField(User, on_delete=models.CASCADE)
+		# username			(required, string)
+		# first_name			(optional, string)
+		# last_name			(optional, string)
+		# email				(optional, string)
+		# password			(required, string(kinda))
+		# groups				(optional, Many-to-many relationship to Group)
+		# user_permissions	(optional, Many-to-many relationship to Permission)
+		# is_staff			(optional, bool)
+		# is_active			(optional, bool)
+		# is_superuser		(optional, bool)
+		# last_login			(optional, date)
+		# date_joined			(optional, date)
+	
+
+	
