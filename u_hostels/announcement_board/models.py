3486022510c9f6e8ac2ce71e23e8144671fa0be5
FatAndreasbot <guva.andrej.01@gmail.com>
2023-02-16 23:34:35 +0700

    added an endpoint for comments. added some models

--- u_hostels/announcement_board/models.py
+++ u_hostels/announcement_board/models.py
@@ -7,10 +7,12 @@ class Post(models.Model):
 	title = models.CharField(max_length=150, blank=True, default="")
 	author = models.ForeignKey(common.Profile, on_delete=models.CASCADE)
 	created = models.DateTimeField(auto_now_add=True)
-	#i should rework attachments
-	img = models.ImageField(upload_to="announcement_board/images/") #MAYBE add more pics in one post
+	#i should probably rework attachments
+	img = models.ImageField(upload_to="announcement_board/images/", blank=True) #MAYBE add more pics in one post
 	is_deleted = models.BooleanField(default=False)
-	tags = models.ManyToManyField("announcementboard.Tags")
+	tags = models.ManyToManyField("announcement_board.Tags")
+
+
 
 	def __str__(self) -> str:
 		return self.title
@@ -24,5 +26,11 @@ class Comment(models.Model):
 	def __str__(self):
 		return (str(self.post) + '\n' + str(self.text))
 
+
 class Tags(models.Model):
 	name = models.CharField(max_length=50)
+
+	def __str__(self) -> str:
+		return self.name
+
+		
